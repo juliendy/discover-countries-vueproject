bf80af6dedd92da3b8ba637610157360d9ac6cd9
juliendy <julien.yasar@gmail.com>
2022-09-04 21:22:15 +0200

    cards display and search bar added + styling.

---
+++ components/CardDisplay.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <country-card
+      :countriesToDisplay="resultcountries"
+      @hide-search-filter="hideSearchFilter"
+      @show-search-filter="showSearchFilter"
+    >
+    </country-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resultcountries: {
+      type: Array,
+    },
+  },
+  methods: {
+    hideSearchFilter(valueFalse) {
+      this.$emit("hide-search-filter", valueFalse);
+    },
+    showSearchFilter(valueTrue) {
+      this.$emit("show-search-filter", valueTrue);
+    },
+  },
+};
+</script>
+<style>
+.card .countries-cards-container li {
+  list-style: none;
+  box-shadow: var(--main-shadow);
+  border-radius: var(--border-radius-img-card);
+  cursor: pointer;
+  padding-bottom: 1em;
+  background: hsl(0, 0%, 100%);
+}
+
+.countries-cards-container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 4em 1em;
+  margin: 0 auto;
+}
+
+.countries-informations {
+  height: 50%;
+  padding: 1em;
+}
+
+.countries-informations > * {
+  margin-bottom: 0.4em;
+}
+
+.countries-informations h2 {
+  font-weight: bold;
+  font-size: 1.4em;
+  margin: 0 0 0.6em 0;
+}
+
+@media screen and (min-width: 300px) {
+  .countries-cards-container {
+    justify-content: center;
+    padding: 0;
+  }
+}
+
+@media screen and (min-width: 800px) {
+  .countries-cards-container {
+    gap: 4em 1.5em;
+  }
+}
+@media screen and (min-width: 950px) {
+  .countries-cards-container {
+    gap: 4em 2.5em;
+  }
+}
+</style>
---
+++ components/SearchBar.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="search-container">
+    <i class="fas fa-search"></i>
+    <input
+      v-model="searchInput"
+      @input="searchSubmit"
+      class="search-country"
+      type="text"
+      id="search-bar"
+      placeholder="Search..."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchInput: "",
+    };
+  },
+  methods: {
+    searchSubmit() {
+      this.$emit("search-submit", this.searchInput.toLowerCase());
+    },
+  },
+};
+</script>
+
+<style>
+.searchbar-filter-container {
+  padding: 2em 3em;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+
+.search-container input {
+  border: none;
+  height: 3em;
+  outline: none;
+  width: 95%;
+}
+
+input:focus::placeholder {
+  color: transparent;
+}
+
+.search-container {
+  padding: 0 1em;
+  width: 40%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: var(--border-radius-img-card);
+  box-shadow: var(--main-shadow);
+  background: hsl(0, 0%, 100%);
+}
+.search-country {
+  position: relative;
+}
+
+.dark-mode--active .search-container,
+.dark-mode--active .search-container input {
+  background-color: hsl(210, 22%, 22%);
+  color: hsl(0, 0%, 100%);
+}
+@media screen and (min-width: 300px) {
+  .searchbar-filter-container {
+    display: block;
+    margin: 0 auto;
+    position: sticky;
+    top: 3.5em;
+    background-color: rgb(255, 255, 255);
+  }
+
+  .search-container {
+    width: 100%;
+    margin-bottom: 1em;
+  }
+
+  .filter-select {
+    width: 10em;
+  }
+}
+
+@media screen and (min-width: 800px) {
+  .searchbar-filter-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 2em;
+  }
+
+  .search-container {
+    width: 60%;
+    margin-left: 2em;
+  }
+
+  .filter-select {
+    margin-right: 2em;
+  }
+}
+
+@media screen and (min-width: 1100px) {
+  .search-container {
+    margin-left: 2em;
+  }
+}
+</style>
